5ac5105bb74f096b2ea03876b998d09991611e05
cy <1121643803@qq.com>
2016-03-16 10:16:14 +0800

    搭建框架

---
+++ woshixiangmujingli/woshixiangmujingli/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  woshixiangmujingli
+//
+//  Created by lanou on 16/3/16.
+//  Copyright © 2016年 阳阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ woshixiangmujingli/woshixiangmujingli/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  woshixiangmujingli
+//
+//  Created by lanou on 16/3/16.
+//  Copyright © 2016年 阳阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
